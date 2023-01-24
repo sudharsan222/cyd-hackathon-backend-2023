e86c9260f2d8ff4c1f8b526b31733b7e8db3a2a3
Jmarc369 <jmarc@cypherd.io>
2023-01-24 20:42:27 +0530

    feat: lowdb req func

--- src/shared/db.service.ts
+++ src/shared/db.service.ts
@@ -3,25 +3,30 @@ https://docs.nestjs.com/providers#services
 */
 
 import { Injectable } from '@nestjs/common';
+import { get, set } from 'lodash';
 import { Low } from 'lowdb';
-// import lodash from 'lodash';
 import { JSONFile } from 'lowdb/lib/node';
 
 @Injectable()
 export class DbService {
+  private db: Low;
   constructor() {
     const adapter = new JSONFile('db.json');
-    const db = new Low(adapter);
-    db.read();
+    this.db = new Low(adapter);
   }
 
-  // async read(key: string) {
+  async read(key: string) {
+    await this.db.read();
+    return get(this.db.data, key);
+  }
 
-  // }
-
-  // async write(key: string, data: any) {
-
-  // }
+  async write(key: string, data: any) {
+    const dataObj = {};
+    set(dataObj, key, data);
+    this.db.data = dataObj;
+    await this.db.write();
+    return this.db.data;
+  }
 
   // async update(key: string, data: any) {
 
